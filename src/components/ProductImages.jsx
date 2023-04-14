c68ef71085503ed51e7530f7c50be1eff6ef7db5
burak <Reayhs1@gmail.com>
2023-04-14 16:58:26 +0300

    ProductImages style has changed

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -4,11 +4,11 @@ function ProductImages({ images = [{ url: "" }] }) {
   const [main, setMain] = useState(images[0]);
   return (
     <div className="">
-      <img src={main.url} alt="img" className="w-full  h-[400px] lg:h-full " />
+      <img src={main.url} alt="img" className="w-[475px] h-[500px]" />
       <div className="flex  mt-6 gap-4 cursor-pointer ">
         {images.map((item, i) => (
           <img
-            className={`w-[74px] lg:w-[94px]  tl:w-14 rounded-lg ${main !== images[i] ? "opacity-25" : null}`}
+            className={`w-[82px] h-[74px] lg:w-[48px]  tl:w-12 tl:h-[56px] rounded-lg  ${main !== images[i] ? "opacity-25" : null}`}
             src={item.url}
             alt="img"
             onClick={() => setMain(images[i])}
